6c000eeb8d95d99ab2bab2e65dab5d4dc4711cce
sanjayms <sanjaymsdev00@gmail.com>
2024-04-11 17:16:48 +0530

    file deleted

--- src/app/environment/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    // api: '7cc5349404msh3481a625d21b2f2p1a83fejsnb25ee98dd18a',
-    key: 'b142624ff7mshc9eb85a2a146a60p1eca77jsncca44e227375',
-    host: 'open-weather13.p.rapidapi.com',
-    baseUrl: 'https://open-weather13.p.rapidapi.com/city/'
-}
